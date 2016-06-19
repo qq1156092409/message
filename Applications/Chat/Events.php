02bc850fa7eb227c7e4496c1419074e3c72d5aa8
qq1156092409 <1156092409@qq.com>
2016-06-19 22:12:55 +0800

    ignore

--- Applications/Chat/Events.php
+++ Applications/Chat/Events.php
@@ -21,59 +21,54 @@
 
 /**
  * 聊天主逻辑
- * 主要是处理 onMessage onClose 
+ * 主要是处理 onMessage onClose
  */
 use \GatewayWorker\Lib\Gateway;
 
 class Events
 {
-   
-   /**
-    * 有消息时
-    * @param int $client_id
-    * @param mixed $message
-    */
-   public static function onMessage($client_id, $message)
-   {
+
+    /**
+     * 有消息时
+     * @param int $client_id
+     * @param mixed $message
+     */
+    public static function onMessage($client_id, $message)
+    {
         // 客户端传递的是json数据
         $message_data = json_decode($message, true);
-        if(!$message_data)
-        {
-            return ;
+        if (!$message_data) {
+            return;
         }
-        
+
         // 根据类型执行不同的业务
-        switch($message_data['type'])
-        {
+        switch ($message_data['type']) {
             // 客户端回应服务端的心跳
             case 'pong':
                 break;
             case 'init':
                 Gateway::bindUid($client_id, $message_data["user"]);
                 Gateway::joinGroup($client_id, $message_data["group"]);
-                $data=array("type"=>"init","user"=>$message_data["user"],"group"=>$message_data["group"]);
+                $data = array("type" => "init", "user" => $message_data["user"], "group" => $message_data["group"]);
                 Gateway::sendToCurrentClient(json_encode($data));
                 break;
-            default:break;
+            default:
+                break;
         }
-   }
-   
-   /**
-    * 当客户端断开连接时
-    * @param integer $client_id 客户端id
-    */
-   public static function onClose($client_id)
-   {
-       // debug
-       echo "client:{$_SERVER['REMOTE_ADDR']}:{$_SERVER['REMOTE_PORT']} gateway:{$_SERVER['GATEWAY_ADDR']}:{$_SERVER['GATEWAY_PORT']}  client_id:$client_id onClose:''\n";
-       
-       // 从房间的客户端列表中删除
-       if(isset($_SESSION['room_id']))
-       {
-           $room_id = $_SESSION['room_id'];
-           $new_message = array('type'=>'logout', 'from_client_id'=>$client_id, 'from_client_name'=>$_SESSION['client_name'], 'time'=>date('Y-m-d H:i:s'));
-           Gateway::sendToGroup($room_id, json_encode($new_message));
-       }
-   }
-  
+    }
+
+    /**
+     * 当客户端断开连接时
+     * @param integer $client_id 客户端id
+     */
+    public static function onClose($client_id)
+    {
+        // 从房间的客户端列表中删除
+        if (isset($_SESSION['room_id'])) {
+            $room_id = $_SESSION['room_id'];
+            $new_message = array('type' => 'logout', 'from_client_id' => $client_id, 'from_client_name' => $_SESSION['client_name'], 'time' => date('Y-m-d H:i:s'));
+            Gateway::sendToGroup($room_id, json_encode($new_message));
+        }
+    }
+
 }
